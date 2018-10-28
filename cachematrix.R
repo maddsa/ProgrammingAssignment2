552e911a36442267bee9c05efef8c0e6aebf1815
maddsa <baba.maddula@gmail.com>
2018-10-28 17:06:00 +0530

    checking in changes for lexical scoping assignment for matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +20,30 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
 }
+
+##  m <- matrix(rnorm(9),3,3)
+##  m3 <- makeCacheMatrix(m)
+##  cacheSolve(m3)
+##        [,1]       [,2]       [,3]
+##  [1,] 0.5731205 -0.5333043 -2.2725906
+##  [2,] 0.8065996 -0.2105556 -1.3881825
+##  [3,] 0.2243270 -0.5488767 -0.5919234
+##  cacheSolve(m3)
+##  getting cached data
+##        [,1]       [,2]       [,3]
+##  [1,] 0.5731205 -0.5333043 -2.2725906
+##  [2,] 0.8065996 -0.2105556 -1.3881825
+##  [3,] 0.2243270 -0.5488767 -0.5919234
+
+
+
